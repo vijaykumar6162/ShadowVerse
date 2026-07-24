58ab8ba229ba9fd19abb9f41edb3362e0375f067
vijaykumar6162 <vijaykumarmandal01106162@gmail.com>
2026-07-24 08:21:38 +0530

    Implement loader fade out on window load
    Add a loader that fades out on window load.

--- loader.js
+++ loader.js
@@ -1,1 +1,18 @@
+/* =========================================
+   SHADOWVERSE
+   LOADER
+========================================= */
 
+window.addEventListener("load", () => {
+
+    const loader = document.getElementById("loader");
+
+    // Loader Fade Out
+
+    loader.style.opacity = "0";
+
+    loader.style.visibility = "hidden";
+
+    loader.style.transition = "1s ease";
+
+}); 
