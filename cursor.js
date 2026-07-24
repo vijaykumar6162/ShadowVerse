3eb2b4acf4a32219e3ce0818ae1bf4bd60cd20f0
vijaykumar6162 <vijaykumarmandal01106162@gmail.com>
2026-07-24 08:13:27 +0530

    Add custom cursor movement and resizing effects
    Implement custom cursor behavior for buttons and links.

--- cursor.js
+++ cursor.js
@@ -1,1 +1,59 @@
+/* =========================================
+   SHADOWVERSE
+   CUSTOM CURSOR
+========================================= */
 
+const cursor = document.querySelector(".cursor");
+
+// Cursor Move
+
+document.addEventListener("mousemove", (e) => {
+
+    cursor.style.left = e.clientX + "px";
+    cursor.style.top = e.clientY + "px";
+
+});
+
+// Cursor Grow on Button
+
+const allButtons = document.querySelectorAll("button");
+
+allButtons.forEach((button) => {
+
+    button.addEventListener("mouseenter", () => {
+
+        cursor.style.width = "35px";
+        cursor.style.height = "35px";
+
+    });
+
+    button.addEventListener("mouseleave", () => {
+
+        cursor.style.width = "18px";
+        cursor.style.height = "18px";
+
+    });
+
+});
+
+// Cursor Grow on Links
+
+const allLinks = document.querySelectorAll("a");
+
+allLinks.forEach((link) => {
+
+    link.addEventListener("mouseenter", () => {
+
+        cursor.style.width = "30px";
+        cursor.style.height = "30px";
+
+    });
+
+    link.addEventListener("mouseleave", () => {
+
+        cursor.style.width = "18px";
+        cursor.style.height = "18px";
+
+    });
+
+});
