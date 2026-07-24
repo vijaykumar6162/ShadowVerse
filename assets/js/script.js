555fa4e16250d1cc8f570af5737897ff768cae5d
vijaykumar6162 <vijaykumarmandal01106162@gmail.com>
2026-07-24 07:30:05 +0530

    Implement custom cursor on mouse move
    Add custom cursor that follows mouse movement.

--- assets/js/script.js
+++ assets/js/script.js
@@ -86,3 +86,12 @@ btn.style.transform="scale(1)";
 });
 
 });
+const cursor=document.querySelector(".cursor");
+
+document.addEventListener("mousemove",(e)=>{
+
+cursor.style.left=e.clientX+"px";
+
+cursor.style.top=e.clientY+"px";
+
+});
